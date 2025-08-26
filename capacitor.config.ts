c03420badd99d8c2999dfaca8098f43af04673cd
semtetog <brennodmoraes@gmail.com>
2025-08-26 09:10:43 -0300

    Fix: remove a propriedade 'bundledWebRuntime' do capacitor.config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -14,6 +14,12 @@ const config: CapacitorConfig = {
       launchShowDuration: 2000,
       launchAutoHide: true,
       backgroundColor: "#101212",
+    },
+    // Configuração da Status Bar para resolver o problema de sobreposição
+    StatusBar: {
+      style: 'dark', // Deixa o texto da status bar (horas, bateria) branco
+      overlay: false, // Esta é a linha mais importante: move o app para baixo da status bar
+      backgroundColor: '#101212' // Define a cor da área da status bar para combinar com seu app
     }
   }
 };
